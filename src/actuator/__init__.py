b7848328120c98e6eadd302f05b44c7549aaa05c
Tom Carroll <tcarroll@pobox.com>
2014-10-30 15:04:21 +0000

    added the ability to use context expressions as the value of a Var object

--- src/actuator/__init__.py
+++ src/actuator/__init__.py
@@ -23,7 +23,8 @@
 from modeling import MultiComponent, MultiComponentGroup, ComponentGroup, ctxt, ActuatorException
 from infra import (InfraSpec, InfraException, with_infra_components)
 from namespace import (Var, NamespaceSpec, with_variables, NamespaceException, Component,
-                                    with_components)
+                       with_components, NSMultiComponent, NSComponentGroup,
+                       NSMultiComponentGroup)
 from config import (ConfigSpec, with_searchpath, with_dependencies, MakeDir, Template,
                     CopyAssets, ConfigJob, ConfigException, TaskGroup, NullTask)
 from provisioners.core import ProvisionerException
