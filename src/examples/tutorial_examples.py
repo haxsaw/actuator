6115294c10788e106eaafd14af127f0b80b4b83c
Tom Carroll <tcarroll@pobox.com>
2015-02-10 10:53:27 +0000

    renaming and re-arranging the examples file

--- src/examples/tutorial_examples.py
+++ src/examples/tutorial_examples.py
@@ -19,17 +19,15 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-'''
-Created on 9 Sep 2014
-'''
-from actuator import (InfraModel, MultiComponent, MultiComponentGroup, ctxt,
+from actuator import (InfraModel, MultiResource, MultiResourceGroup, ctxt,
                       with_roles, NamespaceModel, Role, Var, with_variables,
-                      with_resources, ComponentGroup)
+                      with_resources, ResourceGroup)
 from actuator.provisioners.openstack.resources import (Server, Network, Subnet,
                                                          FloatingIP, Router,
                                                          RouterGateway,
                                                          RouterInterface)
 
+# Simple Openstack example
 class SingleOpenstackServer(InfraModel):
     server = Server("actuator1", "Ubuntu 13.10", "m1.small",
                     nics=[ctxt.model.net])  # @UndefinedVariable
@@ -48,8 +46,15 @@ class SingleOpenstackServer(InfraModel):
     rinter = RouterInterface("actuator_ex1_rinter",
                              ctxt.model.router,  # @UndefinedVariable
                              ctxt.model.subnet)  # @UndefinedVariable
+    
+# if you get some credentials on an Openstack instance you can try out the following:
+# from actuator.provisioners.openstack.openstack import OpenstackProvisioner
+# inst = SingleOpenstackServer("actuator_ex1")
+# provisioner = OpenstackProvisioner(uid, pwd, uid, url)
+# provisioner.provision_infra_spec(inst)
   
-  
+
+#repeat of first example with common components factored out
 gateway_components = {"net":Network("actuator_ex1_net"),
                       "subnet":Subnet("actuator_ex1_subnet",
                                       ctxt.model.net,  # @UndefinedVariable
@@ -72,29 +77,7 @@ class SingleOpenstackServer2(InfraModel):
                      pool="external")
   
   
-gateway_component = ComponentGroup("gateway", net=Network("actuator_ex1_net"),
-                                   subnet=Subnet("actuator_ex1_subnet",
-                                                 ctxt.comp.container.net,
-                                                 "192.168.23.0/24",
-                                                 dns_nameservers=['8.8.8.8']),
-                                   router=Router("actuator_ex1_router"),
-                                   gateway=RouterGateway("actuator_ex1_gateway",
-                                                         ctxt.comp.container.router,
-                                                         "external"),
-                                   rinter=RouterInterface("actuator_ex1_rinter",
-                                                          ctxt.comp.container.router,
-                                                          ctxt.comp.container.subnet))
-
-
-class SingleOpenstackServer3(InfraModel):
-    gateway = gateway_component
-    server = Server("actuator1", "Ubuntu 13.10", "m1.small",
-                    nics=[ctxt.model.gateway.net])  # @UndefinedVariable
-    fip = FloatingIP("actuator_ex1_float", ctxt.model.server,  # @UndefinedVariable
-                     ctxt.model.server.iface0.addr0,  # @UndefinedVariable
-                     pool="external")
-  
-
+# MultipleServers tutorial example
 class MultipleServers(InfraModel):
     #
     #First, declare the common networking components with with_infra_components
@@ -107,16 +90,39 @@ class MultipleServers(InfraModel):
     #
     foreman = Server("foreman", "Ubuntu 13.10", "m1.small",
                      nics=[ctxt.model.net])  # @UndefinedVariable
-    fip = FloatingIP("actuator_ex2_float", ctxt.model.server,  # @UndefinedVariable
-                     ctxt.model.server.iface0.addr0,  # @UndefinedVariable
+    fip = FloatingIP("actuator_ex2_float", ctxt.model.foreman,  # @UndefinedVariable
+                     ctxt.model.foreman.iface0.addr0,  # @UndefinedVariable
                      pool="external")
     #
     #finally, declare the workers MultiComponent
     #
-    workers = MultiComponent(Server("worker", "Ubuntu 13.10", "m1.small",
+    workers = MultiResource(Server("worker", "Ubuntu 13.10", "m1.small",
                                     nics=[ctxt.model.net]))  # @UndefinedVariable
   
+
+# Resource groups example
+gateway_component = ResourceGroup("gateway", net=Network("actuator_ex1_net"),
+                                   subnet=Subnet("actuator_ex1_subnet",
+                                                 ctxt.comp.container.net,
+                                                 "192.168.23.0/24",
+                                                 dns_nameservers=['8.8.8.8']),
+                                   router=Router("actuator_ex1_router"),
+                                   gateway=RouterGateway("actuator_ex1_gateway",
+                                                         ctxt.comp.container.router,
+                                                         "external"),
+                                   rinter=RouterInterface("actuator_ex1_rinter",
+                                                          ctxt.comp.container.router,
+                                                          ctxt.comp.container.subnet))
+
+class SingleOpenstackServer3(InfraModel):
+    gateway = gateway_component
+    server = Server("actuator1", "Ubuntu 13.10", "m1.small",
+                    nics=[ctxt.model.gateway.net])  # @UndefinedVariable
+    fip = FloatingIP("actuator_ex1_float", ctxt.model.server,  # @UndefinedVariable
+                     ctxt.model.server.iface0.addr0,  # @UndefinedVariable
+                     pool="external")
   
+
 class MultipleGroups(InfraModel):
     #
     #First, declare the common networking components
@@ -136,10 +142,10 @@ class MultipleGroups(InfraModel):
     #finally, declare a "cluster"; a leader that coordinates the workers in the
     #cluster, which operate under the leader's direction
     #
-    cluster = MultiComponentGroup("cluster",
+    cluster = MultiResourceGroup("cluster",
                                   leader=Server("leader", "Ubuntu 13.10", "m1.small",
                                                 nics=[ctxt.model.net]),  # @UndefinedVariable
-                                  workers=MultiComponent(Server("cluster_node",
+                                  workers=MultiResource(Server("cluster_node",
                                                                 "Ubuntu 13.10",
                                                                 "m1.small",
                                                                 nics=[ctxt.model.net])))  # @UndefinedVariable
