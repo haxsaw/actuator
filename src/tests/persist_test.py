4cb6127504b142f25aca58f1be801d377ff6a200
Tom Carroll <tcarroll@pobox.com>
2015-06-05 16:05:18 +0100

    Loads of tests of TaskGroups (and implicitly of Orables and Dependencies) in persisting/reanimating.

--- src/tests/persist_test.py
+++ src/tests/persist_test.py
@@ -114,6 +114,126 @@ def test05():
             tgp.args[2].from_task.name == "t1" and
             tgp.args[2].to_task.name == "t3")
 
+def test06():
+    """
+    test06: TaskGroup with no dependencies
+    """
+    t1 = Task("t1")
+    t2 = Task("t2")
+    t3 = Task("t3")
+    tg = TaskGroup(t1, t2, t3)
+    d = persist_to_dict(tg)
+    d_json = json.dumps(d)
+    d = json.loads(d_json)
+    tgp = reanimate_from_dict(d)
+    assert (len(tgp.args) == 3 and
+            len(tgp.unpack()) == 0 and
+            tgp.args[0].name == "t1" and
+            tgp.args[1].name == "t2" and
+            tgp.args[2].name == "t3")
+
+def test06():
+    """
+    test06: TaskGroup with serial and parallel tasks
+    """
+    t1 = Task("t1")
+    t2 = Task("t2")
+    t3 = Task("t3")
+    tg = TaskGroup(t1 | (t2 & t3))
+    d = persist_to_dict(tg)
+    d_json = json.dumps(d)
+    d = json.loads(d_json)
+    tgp = reanimate_from_dict(d)
+    unpacked = tgp.unpack()
+    assert (len(tgp.args) == 1 and
+            len(unpacked) == 2 and
+            unpacked[0].from_task.name == "t1" and
+            unpacked[1].from_task.name == "t1" and
+            unpacked[0].to_task.name == "t2" and
+            unpacked[1].to_task.name == "t3")
+
+def test07():
+    """
+    test07: TaskGroup with parallel and serial tasks
+    """
+    t1 = Task("t1")
+    t2 = Task("t2")
+    t3 = Task("t3")
+    tg = TaskGroup(t1 & (t2 | t3))
+    d = persist_to_dict(tg)
+    d_json = json.dumps(d)
+    d = json.loads(d_json)
+    tgp = reanimate_from_dict(d)
+    unpacked = tgp.unpack()
+    assert (len(tgp.args) == 1 and
+            len(unpacked) == 1 and
+            unpacked[0].from_task.name == "t2" and
+            unpacked[0].to_task.name == "t3" and
+            tgp.args[0].args[0].name == "t1" and
+            isinstance(tgp.args[0].args[1], _Dependency))
+    
+def test08():
+    """
+    test08: TaskGroup with repeated tasks
+    """
+    t1 = Task("t1")
+    t2 = Task("t2")
+    t3 = Task("t3")
+    tg = TaskGroup(t1 | t2, t1 | t3)
+    d = persist_to_dict(tg)
+    d_json = json.dumps(d)
+    d = json.loads(d_json)
+    tgp = reanimate_from_dict(d)
+    unpacked = tgp.unpack()
+    assert (len(tgp.args) == 2 and
+            len(unpacked) == 2 and
+            unpacked[0].from_task.name == "t1" and
+            unpacked[1].from_task.name == "t1" and
+            unpacked[0].to_task.name == "t2" and
+            unpacked[1].to_task.name == "t3")
+    
+def test09():
+    """
+    test09: TaskGroup with dependency cycle; this shouldn't be possible
+    """
+    t1 = Task("t1")
+    t2 = Task("t2")
+    t3 = Task("t3")
+    tg = TaskGroup(t1 | t2 | t3 | t1)
+    d = persist_to_dict(tg)
+    d_json = json.dumps(d)
+    d = json.loads(d_json)
+    tgp = reanimate_from_dict(d)
+    unpacked = tgp.unpack()
+    assert (len(tgp.args) == 1 and
+            len(unpacked) == 3 and
+            unpacked[0].from_task.name == "t1" and
+            unpacked[0].to_task.name == "t2" and
+            unpacked[1].from_task.name == "t2" and
+            unpacked[1].to_task.name == "t3" and
+            unpacked[2].from_task.name == "t3" and
+            unpacked[2].to_task.name == "t1")
+
+def test10():
+    """
+    test10: Complex task group with loads o' tasks
+    """
+    for i in range(1, 11):
+        exec "t%d = Task('t%d')" % (i, i)
+    tg = TaskGroup(t1 | (t2 & (t3 | t4)),
+                   t3 | ((t5 | t6) & t7),
+                   (t6 & t8) | (t9 & t10))
+    d = persist_to_dict(tg)
+    d_json = json.dumps(d)
+    d = json.loads(d_json)
+    tgp = reanimate_from_dict(d)
+    unpacked = tgp.unpack()
+    assert (len(unpacked) == 10 and
+            set([(d.from_task.name, d.to_task.name) for d in unpacked]) ==
+            set([("t1", "t2"), ("t1", "t3"), ("t3", "t4"),
+                 ("t3", "t5"), ("t3", "t7"), ("t5", "t6"),
+                 ("t6", "t9"), ("t6", "t10"), ("t8", "t9"),
+                 ("t8", "t10")]))
 
 def test13():
     r = Role("wibble")
